6173a7b4474bf75fd46e2b9e8c7d4bd5d6d33bcb
Mason Barnes <barnesmason980@gmail.com>
2024-08-05 15:34:17 -0700

    Lab Complete

--- server/models.py
+++ server/models.py
@@ -11,22 +11,70 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+    
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
+    
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "reviews": self.reviews
+        }
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+    customers = association_proxy('reviews','customer')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+        
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "reviews": self.reviews
+        }
+    
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates=('reviews'))
+    item = db.relationship('Item', back_populates=('reviews'))
+
+
+    
+    def __repr__(self):
+        return f'<Item {self.id}, {self.comment}, {self.customer_id, self.item_id}>'
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "comment": self.comment,
+            "customer_id": self.customer_id,
+            "item_id": self.item_id,
+            "customer": {"id": self.customer.id, "name": self.customer.name},
+            "item": {"id": self.item.id, "name": self.item.name}
+        }
